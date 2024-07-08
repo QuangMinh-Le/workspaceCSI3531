8067429fc38b5170c651c1c6895d2385cf40f3fd
Minh <quangminh.ottawa@gmail.com>
2024-07-08 02:48:43 -0400

    assignment 3 done

---
+++ Assignment 3/README.md
@@ -0,0 +1,5 @@
+Assignment 3 done by Group 7:
+
+Quang Minh Le - 300165003
+
+Mohamed Mouad Lahmam - 
--- Assignment 3/SleepingTeachingAssistant.java
+++ Assignment 3/SleepingTeachingAssistant.java
@@ -8,8 +8,8 @@ import java.util.Random;
 public class SleepingTeachingAssistant {
 
     // Number of students and chairs
-    private static final int NUM_STUDENTS = 5;
-    private static final int NUM_CHAIRS = 3;
+    private static final int NUMBER_STUDENTS = 6;
+    private static final int NUMBER_CHAIRS = 3;
 
     // Semaphore for waking up the TA
     private static final Semaphore taSemaphore = new Semaphore(0);
@@ -20,11 +20,14 @@ public class SleepingTeachingAssistant {
     // Queue to keep track of waiting students
     private static final Queue<Integer> waitingStudents = new LinkedList<>();
 
+    // Counter to track the number of students needing help (assume that each student only ask for help once)
+    private static int studentsNeedingHelp = NUMBER_STUDENTS;
+
     public static void main(String[] args) {
         Thread taThread = new Thread(new TA());
         taThread.start();
 
-        for (int i = 0; i < NUM_STUDENTS; i++) {
+        for (int i = 0; i < NUMBER_STUDENTS; i++) {
             Thread studentThread = new Thread(new Student(i));
             studentThread.start();
         }
@@ -38,28 +41,34 @@ public class SleepingTeachingAssistant {
                     // TA waits to be woken up by a student
                     taSemaphore.acquire();
 
-                    int studentId;
-                    queueLock.lock();
-                    try {
-                        studentId = waitingStudents.poll();
-                    } finally {
-                        queueLock.unlock();
-                    }
-
-                    if (studentId != -1) {
-                        System.out.println("TA is helping student " + studentId);
-                        // Simulate helping the student
-                        Thread.sleep(new Random().nextInt(3000) + 1000);
-                        System.out.println("TA finished helping student " + studentId);
-                    }
-
+                    Integer studentId = null;
                     queueLock.lock();
                     try {
                         if (!waitingStudents.isEmpty()) {
-                            taSemaphore.release();  // Wake up again to help the next student
+                            studentId = waitingStudents.poll();
                         }
                     } finally {
                         queueLock.unlock();
+                    }
+
+                    if (studentId != null) {
+                        System.out.println("TA is helping student " + studentId);
+                        // Simulate time helping the student
+                        Thread.sleep(new Random().nextInt(2222) + 1000);
+                        System.out.println("TA finished helping student " + studentId);
+
+                        queueLock.lock();
+                        try {
+                            studentsNeedingHelp--;
+                            if (studentsNeedingHelp == 0 && waitingStudents.isEmpty()) {
+                                System.out.println("TA done helping all students. TA goes back to nap.");
+                                break;
+                            } else if (!waitingStudents.isEmpty()) {
+                                taSemaphore.release();  // Wake up again to help the next student
+                            }
+                        } finally {
+                            queueLock.unlock();
+                        }
                     }
 
                 } catch (InterruptedException e) {
@@ -81,23 +90,30 @@ public class SleepingTeachingAssistant {
             while (true) {
                 try {
                     // Simulate programming
-                    Thread.sleep(new Random().nextInt(5000) + 1000);
+                    Thread.sleep(new Random().nextInt(3333) + 1000);
                     System.out.println("Student " + studentId + " needs help");
+
+                    boolean gotHelp = false;
 
                     queueLock.lock();
                     try {
-                        if (waitingStudents.size() < NUM_CHAIRS) {
+                        if (waitingStudents.size() < NUMBER_CHAIRS) {
                             System.out.println("Student " + studentId + " is waiting in the hallway");
                             waitingStudents.add(studentId);
                             taSemaphore.release();  // Wake up the TA if necessary
+                            gotHelp = true;
                         } else {
-                            System.out.println("No chairs available. Student " + studentId + " will try later");
+                            System.out.println("No chairs available. Student " + studentId + " will continue programming and will try later");
                         }
                     } finally {
                         queueLock.unlock();
                     }
 
-                    // Wait some time before trying again
+                    if (gotHelp) {
+                        break;
+                    }
+
+                    // Wait some time before trying to ask the TA again again if the student didn't get help
                     Thread.sleep(new Random().nextInt(5000) + 1000);
 
                 } catch (InterruptedException e) {
